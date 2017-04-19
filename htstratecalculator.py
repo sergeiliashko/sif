cc0f833ca0c22e6d66add9ce7d174e82c28d7302
Sergei Liashko <sergei.liashko@gmail.com>
2017-04-19 11:26:20 +0000

    Rate calcs are working. now need to glue it together in main method

--- htstratecalculator.py
+++ htstratecalculator.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+from scipy import stats
 import sympy
+import numpy as np
 from sympy.parsing.sympy_parser import (parse_expr, implicit_multiplication_application,standard_transformations)
 
 # n number of islands
@@ -6,9 +9,9 @@ def constructSymbolicEnergyAndHessian(n):
     def uniti_anisotropy_member(i):
         return f"K_{i}*V_{i}*(sin(th_{i})cos(phi_{i} - phi_K_{i}))**2"
     def uniti_easy_plane_member(i):
-        return f"K_p*V_{i}*cos(th_{i})cos(th_{i})"
+        return f"K_p*V_{i}*cos(th_{i})**2"
     def uniti_zeeman_member(i):
-        return f"M_{i}V_{i}H_{i}sin(th_{i})cos(phi_{i}-phi_H_{i}"
+        return f"M_{i}*V_{i}*H_{i}*sin(th_{i})cos(phi_{i}-phi_H_{i})"
     def uniti_dipole_memeber(i,k):
         _first = f"sin(th_{i})*sin(th_{k})*cos(phi_{i} - phi_{k}) + cos(th_{i})*cos(th_{k})"
         _second = f"3sin(th_{i})cos(phi_{i}-phi_r_{i}{k})sin(th_{k})cos(phi_{k}-phi_r_{i}{k})"
@@ -24,7 +27,7 @@ def constructSymbolicEnergyAndHessian(n):
     for h in range(n):
         _res_anis += "-" + uniti_anisotropy_member(h)
         _res_easyplane += "+" + uniti_easy_plane_member(h)
-        _res_zeeman += "-" + uniti_easy_plane_member(h)
+        _res_zeeman += "-" + uniti_zeeman_member(h)
         for k in range(n):
             if(h<k): _res_dipole += "-" + uniti_dipole_memeber(h,k)
 
@@ -37,11 +40,14 @@ def constructNumericEnergyAndHessian(hessianParams,distances,distance_unit_vecto
     #construct hessian
     _symbolicEnergy, _symbolicHessian = constructSymbolicEnergyAndHessian(n)
 
-
     #theta_r_ij is always pi/2
     _th_r_ij_subs = [(parse_expr(f"th_r_{i}{j}"),sympy.pi/2) for i in range(n) for j in range(n) if i<j]
     _res_hessian = _symbolicHessian.subs(_th_r_ij_subs)
     _res_energy = _symbolicEnergy.subs(_th_r_ij_subs)
+
+    # K_out
+    _res_hessian = _res_hessian.subs([(parse_expr("K_p"),hessianParams["K_out"][0])])
+    _res_energy = _res_energy.subs([(parse_expr("K_p"),hessianParams["K_out"][0])])
 
     #H H_angle
     _field_angles = hessianParams["field_angle"]
@@ -101,37 +107,41 @@ def constructNumericEnergyAndHessian(hessianParams,distances,distance_unit_vecto
     return (_res_energy, _res_hessian)
 
 def calculatePreFactor(numericHessian, minimumCoord, saddleCoord, y):
-    n = minimaCoord.shape[0]
-    hessian_min = numericHessian(minimaCoord)
-    hessian_sad = numericHessian(minimaCoord)
+    n = minimumCoord.shape[0]
+    hessian_min = numericHessian(minimumCoord)
+    hessian_sad = numericHessian(saddleCoord)
     eigen_vals_min = np.linalg.eig(hessian_min)[0]
     eigen_vals_sad, eigen_vecs_sad = np.linalg.eig(hessian_sad)
-    velocities = np.zeros_like(hessian_min)
-    velocities[0:n,0:n*2] = y*he_sad[n:2*n:,0:2*n]
-    velocities[n:2*n,0:n*2] = -y*he_sad[0:n:,0:2*n]
 
-    tmp = np.dot(np.linalg.inv(eig_v_sad),velocities)
+    velocities = np.zeros_like(hessian_min)
+    velocities[0:n,0:n*2] = y*hessian_sad[n:2*n:,0:2*n]
+    velocities[n:2*n,0:n*2] = -y*hessian_sad[0:n:,0:2*n]
+
+    tmp = np.dot(np.linalg.inv(eigen_vecs_sad),velocities)
     tmp = np.dot(tmp, eigen_vecs_sad)
     a_coef = np.copy(tmp[0,:])
-    a[0] = 0.
+    a_coef[0] = 0.
 
     adjusted_eigen_vals_sad = np.copy(eigen_vals_sad)
     adjusted_eigen_vals_sad[0] = 1
 
     det_min = np.sqrt(eigen_vals_min.prod())
-    det_sad = np.sqrt(eigen_vals_sad.prod())
-    pr = np.sqrt((a**2/adjusted_eigen_vals_sad).sum())
+    det_sad = np.sqrt(adjusted_eigen_vals_sad.prod())
+    pr = np.sqrt((a_coef**2/adjusted_eigen_vals_sad).sum())
 
-    pre_exp = (1./2./pi)*pr*(det_min/det_sad)
+    pre_exp = (1./2./np.pi)*pr*(det_min/det_sad)
 
     return pre_exp
 
 def calculateAllPreFactors(numericHessian, minima, saddles,y):
-    n = saddles.shape[1]
-    pre_factors_matrix = np.zeros((n+1,n+1)) # saddles is 1 element lower than minima
-    for i in range(n):
-        pre_factors_matrix[n,n+1] = calculatePreFactor(numericHessian,minima[:,n],saddles[:,n],y)
-        pre_factors_matrix[n+1,n] = calculatePreFactor(numericHessian,minima[:,n+1],saddles[:,n],y)
+    n = minima.shape[1]
+    pre_factors_matrix = np.zeros((n,n))
+    for i in range(n-1):
+        init = minima[:,i]
+        final = minima[:,i+1]
+        barrier = saddles[:,i]
+        pre_factors_matrix[i,i+1] = calculatePreFactor(numericHessian,init,barrier,y)
+        pre_factors_matrix[i+1,i] = calculatePreFactor(numericHessian,final,barrier,y)
     return pre_factors_matrix
 
 def calculateTransitionRateForT(pre_factor,barrier,T,kb):
@@ -143,13 +153,14 @@ def calculateAllTransitionRatesForT(pre_factors_matrix, barriers_matrix, T, kb):
 
 
 # n-1 = number of barriers 
-def calculateOverallRateForT(transition_rates_matrix_for_T, path_coefficients_matrix):
+def calculateOverallRateForT(transition_rates_matrix_for_T, path_coefficients_matrix_1,
+        path_coefficients_matrix_2):
     def b(i):
         if(i==0):
-            return k[0,1]
+            return k1[0,1]
         else:
-            return  k[i,i-1]+k[i,i+1]
-    def a(i): return -k[i-1,i]*k[i,i-1]
+            return  k2[i,i-1]+k1[i,i+1]
+    def a(i): return -k2[i-1,i]*k1[i,i-1]
 
     def A(i):
         if(i==0):
@@ -167,20 +178,25 @@ def calculateOverallRateForT(transition_rates_matrix_for_T, path_coefficients_ma
         else:
             return b(i)*B(i-1)+a(i)*B(i-2)
     n = transition_rates_matrix_for_T.shape[0]
-    k = transition_rates_matrix_for_T*path_coefficients_matrix
-    return A(n-2)/B(n-2)
+    k1 = transition_rates_matrix_for_T*path_coefficients_matrix_1
+    k2 = transition_rates_matrix_for_T*path_coefficients_matrix_2
+    return -(A(n-2)/B(n-2))
 
-def calculateEffectiveBarrierAndPreFactor(temperature_range, path_coefficients_matrix,hessianParams,pre_factors_matrix,barriers_matrix,constants):
+def calculateEffectiveBarrierAndPreFactor(temperature_range,
+        path_coefficients_matrix_1,path_coefficients_matrix_2,
+        hessianParams,pre_factors_matrix,barriers_matrix,constants):
     overall_rates=[]
     kb = constants["boltzmann"]
-    for t in range(len(temperature_range)):
+    for t in temperature_range:
         transition_rates_matrix_for_T = calculateAllTransitionRatesForT(pre_factors_matrix, barriers_matrix, t, kb)
-        calculateOverallRateForT(transition_rates_matrix_for_T,path_coefficients_matrix)
-        overall_rates.append()
+        _overall_r = calculateOverallRateForT(transition_rates_matrix_for_T,path_coefficients_matrix_1,path_coefficients_matrix_2)
+        overall_rates.append(_overall_r)
     temp1 = np.log(np.array(overall_rates))
     slope, intercept, r_value, p_value, std_err = stats.linregress(1./temperature_range,temp1)
     eff_pre_factor = np.exp(intercept)
     eff_barrier = -slope*kb
+    plt.plot(1./temperature_range,temp1 , '-b')
+    plt.show()
     return (eff_barrier,eff_pre_factor)
 
 
@@ -198,13 +214,13 @@ def main(start_T, end_T,step_T,
 
     y = constants["gyromagnetic_ratio"]/(hessianParams["V"]*hessianParams["M"])
     y = np.swapaxes([y],1,0) # I need a vector with the shape (n,1)
-    pre_factor_matrix = calculateAllPreFactors(numericHessian, minimaCoords, saddles,y)
-
+    pre_factors_matrix = calculateAllPreFactors(numericHessian, minimaCoords, saddlesCoords,y)
     _temperature_range = np.linspace(start_T,end_T,step_T)
 
     eff_barrier,eff_pre_factor = calculateEffectiveBarrierAndPreFactor(
             _temperature_range,
-            path_coefficients_matrix,
+            path_coefficients_matrix_1,
+            path_coefficients_matrix_2,
             hessianParams,
             pre_factors_matrix,
             barriers_matrix,
